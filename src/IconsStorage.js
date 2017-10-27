c7d80546e706368305db78d5203c7054cee2fcf2
Ian Andersen <iana@finalweb.com>
2017-10-27 15:56:32 -0700

    fixed some issues

--- src/IconsStorage.js
+++ src/IconsStorage.js
@@ -6,30 +6,19 @@ let instance = null;
 export default class IconsStorage {
 
 	icons;
-	requestWaitingForIcons;
 
 	constructor() {
 		if (instance) {
 			return instance;
 		}
-
-		this.requestWaitingForIcons = [];
 		instance = this;
 	}
 
 	getIcons() {
 		if (this.icons) {
-			return Promise.resolve(this.icons);
+			return this.icons;
 		}
 
-		if (this.isLoadingIcons) {
-			const p = new defer();
-
-			this.requestWaitingForIcons.push(p);
-			return p.promise;
-		}
-
-		this.isLoadingIcons = true;
 		let data = iconDefs.array;
     const icons = data.map(nameAndCode => {
       const parts = nameAndCode.split(':');
@@ -39,13 +28,6 @@ export default class IconsStorage {
       };
     });
     this.icons = icons;
-
-    this.isLoadingIcons = false;
-    if (this.requestWaitingForIcons.length > 0) {
-      this.requestWaitingForIcons.map(awaitingPromise => {
-        awaitingPromise.resolve(this.icons);
-      });
-    }
 
     return icons;
 	}
